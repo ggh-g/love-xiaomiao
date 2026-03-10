6d14cb6b1efdbfc31f4a59da209d63efa31802a3
ggh <@LINGer5201314>
2026-03-10 21:24:52 +0800

    chore: add firebase hosting support

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/love-xiaomiao/",
+  base: process.env.DEPLOY_TARGET === "github-pages" ? "/love-xiaomiao/" : "/",
   plugins: [react(), tailwindcss()],
 });
